429cec1a271ccd7d398d457b1ecfffe0336a3b91
chassis <38101674+Code-Ebullient@users.noreply.github.com>
2022-06-27 23:12:51 +0100

    Update Dbh.php

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -1,3 +1,14 @@
 <?php
+ class Dbh {
+    //Properties
+    private $hostname;
+    private $username;
+    private $password;
+    private $dbname;
 
+    //methods
+    function connect() {
+        
+    }
+ };
 

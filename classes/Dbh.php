cf08e0fd08319bfb11b140fa47357473c319ba2f
chassis <38101674+Code-Ebullient@users.noreply.github.com>
2022-06-28 13:42:18 +0100

    Update Dbh.php

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -6,25 +6,8 @@ class Dbh {
     private $password = "";
     private $dbname = "zuriphp";
 
-    $sql = file_get_contents('users.sql');
-    
-    /* execute multi query */
-    $mysqli->multi_query($sql);
-
-    //$mysqli = new mysqli("localhost", "root", " ", "zuriphp");
-
-    // Create connection
-    $conn = new mysqli($hostname, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-
     //method 
-    protected function connect() {  
-        mysql_connect($this - > hostname, $this - > username, $this - > password, $this - > dbname) or die(mysql_error("zuriphp"));  
-        mysql_select_db($this - > dbname) or die(mysql_error("zuriphp"));  
-    };
+    protected function connect() { 
+        $conn = new mysqli ($this -> hostname, $this -> username, $this -> password, $this -> dbname) or die(mysql_error("zuriphp"));  
+    }
 };
